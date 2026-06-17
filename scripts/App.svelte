fa460c1e623ab733909416a082b1ac063d8107b7
David Bruant <davidbruant@protonmail.com>
2026-06-17 17:42:56 +0200

    style

--- scripts/App.svelte
+++ scripts/App.svelte
@@ -6,6 +6,31 @@
 
 <style lang="scss">
 	
+	:root{
+		--primary-background-color: #090040;
+		--secundary-background-color:  #471396;
+
+		--primary-foreground-color: #FFCC00;
+		--secundary-foreground-color: #B13BFF;
+
+		@media (prefers-color-scheme: light) {
+			
+			--primary-background-color: #DFD0B8;
+			--secundary-background-color: #B13BFF;
+
+			--primary-foreground-color: #090040;
+			--secundary-foreground-color:  #471396;
+
+		}
+
+	}
+
+	:global(body){
+		background-color: var(--primary-background-color);
+		color: var(--primary-foreground-color);
+		border-color: var(--primary-foreground-color);
+	}
+
 	:global(main) {
 		text-align: left;
 		padding: 1em;
@@ -13,12 +38,11 @@
 		margin: 0 auto;
 
 		@media (min-width: 640px) {
-			max-width: none;
+			max-width: 70rem;
 		}
 	}
 
 	h2 {
-		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
